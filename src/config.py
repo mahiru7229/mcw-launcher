ccc15abf315bfcc8b53fa209b1cb01f9146dd2b8
Huỳnh Ngọc Hoàng Tùng <110292918+mahiru7229@users.noreply.github.com>
2026-07-28 01:56:45 +0700

    release: v0.9.0

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-VERSION = "v0.9.0 RC 1"
-VERSION_ID = "0.9.0-rc.1"
+VERSION = "v0.9.0"
+VERSION_ID = "0.9.0"
 VERSION_TAG = f"v{VERSION_ID}"
-UPDATE_CHANNEL = "beta"
+UPDATE_CHANNEL = "stable"
 GITHUB_REPOSITORY = "mahiru7229/mcw-launcher"
 DEVELOPER_NAME = "mahiru7229"
 LAUNCHER_SLUG = "mcw-launcher"
